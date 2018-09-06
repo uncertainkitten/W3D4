4ae98b61f98f7c45403e785cebe947234a4452f7
Cassandra <uncertainkitten@gmail.com>
2018-09-06 16:16:25 -0700

    Create not_polls_app not_schema

---
+++ not_polls_app/db/migrate/20180906231244_create_responses.rb
@@ -0,0 +1,11 @@
+class CreateResponses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :responses do |t|
+      t.integer :user_id, null: false
+      t.integer :answer_id, null: false
+      t.timestamps
+    end
+    add_index :responses, [:answer_id, :user_id], unique: true
+    add_index :responses, :user_id
+  end
+end
